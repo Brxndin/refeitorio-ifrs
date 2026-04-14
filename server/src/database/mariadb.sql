d0eaf6e7df510ca286f4fb081bf2690b353f3928
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-04-14 00:05:41 -0300

    Relação inicial do banco de dados da aplicação

---
+++ server/src/database/mariadb.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE refeitorio_ifrs;
+
+USE refeitorio_ifrs;
+
+CREATE TABLE usuarios(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    senha VARCHAR(200),
+    tipo INT NOT NULL
+);
+
+CREATE TABLE refeicoes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    tipo INT NOT NULL,
+    data DATE NOT NULL
+);
+
+CREATE TABLE refeicoes_itens(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(200) NOT NULL,
+    refeicao_id INT NOT NULL,
+
+    FOREIGN KEY (refeicao_id) REFERENCES refeicoes(id),
+);
+
+-- senha Senha.123
+INSERT INTO usuarios
+(nome, email, senha, tipo)
+VALUES ('Administrador', 'admin@ifrs.edu.br', '$2b$12$fkCox86SvdMTzA7c6XDWLuIEBeq7fPa2ST3Uql8iY5sFkgD/9erXi', 1);
+
+CREATE TABLE recuperacao_senhas(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    token VARCHAR(200) NOT NULL,
+    data_expiracao DATETIME NOT NULL,
+    usuario_id INT NOT NULL,
+
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
