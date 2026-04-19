bfe4bacdd31ac4705b7edae7db8b4e4d143f8551
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-04-18 23:54:35 -0300

    Aplicação de scrollview e safeareaview e melhorias no layout da home

--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -1,25 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Button } from '../components/Button';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
         backgroundColor: '#fff',
     },
-    text: {
+    scrollContent: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
         fontSize: 20,
+        margin: 10,
+    },
+    text: {
+        fontSize: 15,
         margin: 10,
     },
 });
 
 export default function Home({ navigation }) {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Página Inicial</Text>
-            <Button title={'Cardápio'} onPress={() => navigation.navigate('Cardapio')} />
-            <Button title={'Logout'} onPress={() => navigation.navigate('Logout')} />
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollContent}>
+                <Text style={styles.title}>Página Inicial</Text>
+                <Text style={styles.text}>Bem vindo ao sistema de Cardápio do IFRS! Aqui você pode ver o cardápio da semana, fazer a impressão e até mesmo favoritar o seu preferido.</Text>
+                <Button title={'Cardápio'} onPress={() => navigation.navigate('Cardapio')} />
+                <Button title={'Logout'} onPress={() => navigation.navigate('Logout')} />
+            </ScrollView>
+        </SafeAreaView>
     );
 }
