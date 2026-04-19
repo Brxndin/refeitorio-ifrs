a716f7f18f5e4ac2730ab411a7907fdbf0721e9b
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-04-19 17:21:06 -0300

    Ajustes de imports

--- app/src/router.tsx
+++ app/src/router.tsx
@@ -1,6 +1,6 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { useAuth } from './auth/AuthContext';
 import CardapioList from './screens/CardapioList';
 import Home from './screens/Home';
